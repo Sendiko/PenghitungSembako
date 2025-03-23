368e19c2f4232ca3d8cdb158ddb1f41e8a394163
Sendiko <sarangtawon897@gmail.com>
2025-03-23 21:18:56 +0700

    Rename AboutScreen to AboutScreenRoot and add navigation back button to AboutScreen

--- app/src/main/java/com/github/sendiko/penghitungsembako/about/presentation/AboutScreenRoot.kt
+++ app/src/main/java/com/github/sendiko/penghitungsembako/about/presentation/AboutScreenRoot.kt
@@ -2,7 +2,11 @@ package com.github.sendiko.penghitungsembako.about.presentation
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -10,16 +14,39 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.github.sendiko.penghitungsembako.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AboutScreen() {
+fun AboutScreenRoot(navController: NavHostController) {
+    AboutScreen(
+        onNavigateBack = {
+            navController.navigateUp()
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AboutScreen(
+    onNavigateBack: () -> Unit
+) {
     Scaffold(
         topBar = {
             TopAppBar(
                 title = {
                     Text(stringResource(R.string.about))
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = onNavigateBack
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                            contentDescription = null
+                        )
+                    }
                 }
             )
         }
