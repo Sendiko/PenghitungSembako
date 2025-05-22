5e4abac8b321a498d0f78ac2c1bd9ea2ee0ff5c4
Sendiko <sarangtawon897@gmail.com>
2025-05-22 18:21:05 +0700

    feat(networking): add apiservice for user data
    This commit introduces an `ApiService` interface with two methods:
    - `saveUser`: A POST request to create or update a user.
    - `getUser`: A GET request to retrieve user data by ID.

---
+++ app/src/main/java/com/github/sendiko/penghitungsembako/core/network/ApiService.kt
@@ -0,0 +1,24 @@
+package com.github.sendiko.penghitungsembako.core.network
+
+import com.github.sendiko.penghitungsembako.user.data.GetUserResponse
+import com.github.sendiko.penghitungsembako.user.data.SaveUserRequest
+import com.github.sendiko.penghitungsembako.user.data.SaveUserResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface ApiService {
+
+    @POST("user")
+    fun saveUser(
+        @Body request: SaveUserRequest
+    ): Call<SaveUserResponse>
+
+    @GET("user/{id}")
+    fun getUser(
+        @Path("id") id: String,
+    ): Call<GetUserResponse>
+
+}
