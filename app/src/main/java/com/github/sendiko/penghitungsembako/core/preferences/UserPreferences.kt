2fec841ab4bb52285de2fc6c60173b137748a709
Sendiko <sarangtawon897@gmail.com>
2025-05-23 16:46:38 +0700

    feat(user): save and get user data from preferences
    This commit introduces the functionality to save and retrieve user data (username, email, and profile URL) using DataStore preferences.
    
    New `stringPreferencesKey` instances for `nameKey`, `emailKey`, and `profileUrlKey` have been added.
    
    The following functions have been implemented:
    - `saveUser(user: User)`: Saves the user's information to DataStore.
    - `getUser(): Flow<User>`: Retrieves the user's information from DataStore as a Flow.

--- app/src/main/java/com/github/sendiko/penghitungsembako/core/preferences/UserPreferences.kt
+++ app/src/main/java/com/github/sendiko/penghitungsembako/core/preferences/UserPreferences.kt
@@ -4,6 +4,7 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
+import com.github.sendiko.penghitungsembako.user.domain.User
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
@@ -11,7 +12,27 @@ class UserPreferences(
     val dataStore: DataStore<Preferences>
 ) {
 
-    val uiModeKey = stringPreferencesKey("ui_mode")
+    private val uiModeKey = stringPreferencesKey("ui_mode")
+    private val nameKey = stringPreferencesKey("name")
+    private val emailKey = stringPreferencesKey("email")
+    private val profileUrlKey = stringPreferencesKey("profile_url")
+
+    suspend fun saveUser(user: User) {
+        dataStore.edit { preferences ->
+            preferences[nameKey] = user.username
+            preferences[emailKey] = user.email
+            preferences[profileUrlKey] = user.profileUrl
+        }
+    }
+
+    fun getUser(): Flow<User> {
+        return dataStore.data.map { preferences ->
+            val name = preferences[nameKey] ?: ""
+            val email = preferences[emailKey] ?: ""
+            val profileUrl = preferences[profileUrlKey] ?: ""
+            User(name, email, profileUrl)
+        }
+    }
 
     suspend fun setUiMode(uiMode: UiMode) {
         dataStore.edit { preferences ->
