ba304293b63544c3103bdbebdeecb0136862b02f
Sendiko <sarangtawon897@gmail.com>
2025-05-23 19:37:18 +0700

    feat: implement splash screen
    This commit introduces a splash screen to the application using the `installSplashScreen()` method.

--- app/src/main/java/com/github/sendiko/penghitungsembako/core/MainActivity.kt
+++ app/src/main/java/com/github/sendiko/penghitungsembako/core/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.github.sendiko.penghitungsembako.core.navigation.NavGraph
 import com.github.sendiko.penghitungsembako.core.ui.theme.AppTheme
@@ -12,6 +13,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             AppTheme {
                 val navController = rememberNavController()
