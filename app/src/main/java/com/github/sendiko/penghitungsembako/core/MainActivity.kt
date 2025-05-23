20baa44e12f67001edba1b86d9c1d76ec044607d
Sendiko <sarangtawon897@gmail.com>
2025-05-23 16:46:22 +0700

    feat: add new color and typography
    This commit introduces a new color palette and typography settings for the application.
    
    - Updated `Color.kt` with new color definitions for light and dark themes, including medium and high contrast variations.
    - Renamed `PenghitungSembakoTheme` to `AppTheme` in `Theme.kt` and updated it to use the new color schemes.
    - Replaced `Typography` with `AppTypography` in `Type.kt` and defined new font families (Poppins and Plus Jakarta Sans) using Google Fonts.
    - Updated `MainActivity.kt` to use `AppTheme`.

--- app/src/main/java/com/github/sendiko/penghitungsembako/core/MainActivity.kt
+++ app/src/main/java/com/github/sendiko/penghitungsembako/core/MainActivity.kt
@@ -6,14 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.rememberNavController
 import com.github.sendiko.penghitungsembako.core.navigation.NavGraph
-import com.github.sendiko.penghitungsembako.core.ui.theme.PenghitungSembakoTheme
+import com.github.sendiko.penghitungsembako.core.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PenghitungSembakoTheme {
+            AppTheme {
                 val navController = rememberNavController()
                 NavGraph(
                     navController = navController
