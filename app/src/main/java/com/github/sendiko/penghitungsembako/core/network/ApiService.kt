150563816360ee3bed9391c3b2463585019df9c4
Sendiko <sarangtawon897@gmail.com>
2025-05-27 23:09:38 +0700

    chore: remove unused user endpoint
    The GET user endpoint in ApiService has been removed as it is no longer in use.

--- app/src/main/java/com/github/sendiko/penghitungsembako/core/network/ApiService.kt
+++ app/src/main/java/com/github/sendiko/penghitungsembako/core/network/ApiService.kt
@@ -1,13 +1,10 @@
 package com.github.sendiko.penghitungsembako.core.network
 
-import com.github.sendiko.penghitungsembako.login.data.GetUserResponse
-import com.github.sendiko.penghitungsembako.login.data.SaveUserRequest
-import com.github.sendiko.penghitungsembako.login.data.SaveUserResponse
+import com.github.sendiko.penghitungsembako.login.data.dto.SaveUserRequest
+import com.github.sendiko.penghitungsembako.login.data.dto.SaveUserResponse
 import retrofit2.Call
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.Path
 
 interface ApiService {
 
@@ -15,10 +12,5 @@ interface ApiService {
     fun saveUser(
         @Body request: SaveUserRequest
     ): Call<SaveUserResponse>
-
-    @GET("user/{id}")
-    fun getUser(
-        @Path("id") id: String,
-    ): Call<GetUserResponse>
 
 }
