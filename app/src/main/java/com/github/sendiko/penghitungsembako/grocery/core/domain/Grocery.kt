9c0f4e94ef331d39e491338aa164ee9556fad57f
Sendiko <sarangtawon897@gmail.com>
2025-05-29 19:22:43 +0700

    feat(grocery): convert response to model

--- app/src/main/java/com/github/sendiko/penghitungsembako/grocery/core/domain/Grocery.kt
+++ app/src/main/java/com/github/sendiko/penghitungsembako/grocery/core/domain/Grocery.kt
@@ -1,9 +1,21 @@
 package com.github.sendiko.penghitungsembako.grocery.core.domain
 
+import com.github.sendiko.penghitungsembako.grocery.form.data.dto.GroceryItem
+
 data class Grocery(
     val id: Int,
     val name: String,
     val unit: String,
     val pricePerUnit: Int,
     val imageUrl: String,
-)
+) {
+    companion object {
+        fun convertFromResponse(groceryItem: GroceryItem) = Grocery(
+            id = groceryItem.id,
+            name = groceryItem.name,
+            unit = groceryItem.unit,
+            pricePerUnit = groceryItem.price,
+            imageUrl = groceryItem.imageUrl
+        )
+    }
+}
