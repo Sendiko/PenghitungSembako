cc43bac4a81ac592157afe4f97e3685e1f8b922a
Sendiko <sarangtawon897@gmail.com>
2025-05-30 15:49:10 +0700

    feat(grocery): add update grocery data models
    Introduces data classes for updating grocery items:
    - `UpdateGroceryRequest`: Represents the request body for updating a grocery item, including `userId`, `name`, `unit`, `pricePerUnit`, and an optional `image`.
    - `UpdateGroceryResponse`: Represents the response after updating a grocery item, containing `groceryItem`, `message`, and `status`.

---
+++ app/src/main/java/com/github/sendiko/penghitungsembako/grocery/form/data/dto/UpdateGroceryRequest.kt
@@ -0,0 +1,22 @@
+package com.github.sendiko.penghitungsembako.grocery.form.data.dto
+
+import com.google.gson.annotations.SerializedName
+import okhttp3.MultipartBody
+
+data class UpdateGroceryRequest(
+
+    @field:SerializedName("userId")
+    val userId: Int,
+
+    @field:SerializedName("name")
+    val name: String,
+
+    @field:SerializedName("unit")
+    val unit: String,
+
+    @field:SerializedName("price")
+    val pricePerUnit: Double,
+
+    @field:SerializedName("image")
+    val image: MultipartBody.Part? = null,
+)
