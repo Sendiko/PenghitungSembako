928ce59e42b6efbade119b27fdda780c4a95fbed
Sendiko <sarangtawon897@gmail.com>
2025-06-11 23:51:39 +0700

    feat: add HistoryEntity mapping
    Added functions to convert between History and HistoryEntity.
    This allows for easier data handling between domain and data layers.

--- app/src/main/java/id/my/sendiko/sembako/history/domain/History.kt
+++ app/src/main/java/id/my/sendiko/sembako/history/domain/History.kt
@@ -1,5 +1,6 @@
 package id.my.sendiko.sembako.history.domain
 
+import id.my.sendiko.sembako.history.data.HistoryEntity
 import id.my.sendiko.sembako.history.data.dto.HistoryItem
 
 data class History(
@@ -7,12 +8,28 @@ data class History(
     val totalPrice: Int,
     val groceryName: String,
 ) {
+
+    fun toHistoryEntity(): HistoryEntity {
+        return HistoryEntity(
+            quantity = this.quantity,
+            totalPrice = this.totalPrice,
+            groceryName = this.groceryName,
+        )
+    }
     companion object {
         fun fromHistoryItem(historyItem: HistoryItem): History {
             return History(
                 quantity = historyItem.quantity,
                 totalPrice = historyItem.totalPrice,
                 groceryName = historyItem.grocery.name,
+            )
+        }
+
+        fun fromHistoryEntity(historyEntity: HistoryEntity): History {
+            return History(
+                quantity = historyEntity.quantity,
+                totalPrice = historyEntity.totalPrice,
+                groceryName = historyEntity.groceryName,
             )
         }
     }
