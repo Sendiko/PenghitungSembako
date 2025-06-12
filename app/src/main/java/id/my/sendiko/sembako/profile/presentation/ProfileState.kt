1ca04f1618c0477b49817a4a5840ca2d9875756d
Sendiko <sarangtawon897@gmail.com>
2025-06-12 14:46:44 +0700

    chore: Remove statistics from ProfileState
    The `statistics` field in the `ProfileState` data class has been removed. This change simplifies the state representation for the profile screen by removing unused data.

--- app/src/main/java/id/my/sendiko/sembako/profile/presentation/ProfileState.kt
+++ app/src/main/java/id/my/sendiko/sembako/profile/presentation/ProfileState.kt
@@ -1,7 +1,6 @@
 package id.my.sendiko.sembako.profile.presentation
 
 import id.my.sendiko.sembako.core.domain.User
-import id.my.sendiko.sembako.statistics.data.dto.Statistics
 
 data class ProfileState(
     val isSigningOut: Boolean = false,
@@ -10,6 +9,5 @@ data class ProfileState(
     val user: User? = null,
     val dynamicTheme: Boolean = true,
     val isLoading: Boolean = false,
-    val statistics: Statistics? = null,
     val errorMessage: String = ""
 )
