9be992f0f1d383222a1b69154b8f5262c5837811
Sendiko <rizkysendiko7@gmail.com>
2025-08-17 12:04:59 +0700

    chore: Remove unused Room auto-migration
    This commit removes the `autoMigrations` property from the `AppDatabase` definition. The `AutoMigration(from = 4, to = 5)` was likely no longer needed or was handled by other schema migration strategies. Additionally, the import for `androidx.room.AutoMigration` has been removed.

--- app/src/main/java/id/my/sendiko/sembako/core/database/AppDatabase.kt
+++ app/src/main/java/id/my/sendiko/sembako/core/database/AppDatabase.kt
@@ -1,17 +1,15 @@
 package id.my.sendiko.sembako.core.database
 
-import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import id.my.sendiko.sembako.grocery.core.data.GroceryEntity
 import id.my.sendiko.sembako.grocery.core.data.GroceryDao
+import id.my.sendiko.sembako.grocery.core.data.GroceryEntity
 import id.my.sendiko.sembako.history.data.HistoryDao
 import id.my.sendiko.sembako.history.data.HistoryEntity
 
 @Database(
     entities = [GroceryEntity::class, HistoryEntity::class],
     version = 5,
-    autoMigrations = [AutoMigration(from = 4, to = 5)],
     exportSchema = true
 )
 abstract class AppDatabase : RoomDatabase() {
