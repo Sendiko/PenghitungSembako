515e09b3840c9aa278176a4eae49002d5737446b
Sendiko <rizkysendiko7@gmail.com>
2026-05-21 00:23:34 +0700

    chore: update User domain model usage

--- app/src/main/java/id/my/sendiko/sembako/grocery/form/presentation/FormState.kt
+++ app/src/main/java/id/my/sendiko/sembako/grocery/form/presentation/FormState.kt
@@ -20,5 +20,5 @@ data class FormState(
     val imageUrl: String = "",
     val bitmapMessage: String = "",
     val isLoading: Boolean = false,
-    val user: User = User(0, "", "", "")
+    val user: User = User(0, "", "", "", false)
 )
