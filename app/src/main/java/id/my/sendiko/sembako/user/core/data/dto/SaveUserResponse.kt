703bdecb0bbbbc14b56d0fc0e21c804a50c809f0
Sendiko <rizkysendiko7@gmail.com>
2026-05-21 09:52:07 +0700

    chore: moved packages

---
+++ app/src/main/java/id/my/sendiko/sembako/user/core/data/dto/SaveUserResponse.kt
@@ -0,0 +1,43 @@
+package id.my.sendiko.sembako.user.core.data.dto
+
+import com.google.gson.annotations.SerializedName
+import id.my.sendiko.sembako.store.data.dto.StoreDto
+import id.my.sendiko.sembako.user.core.domain.User
+
+data class SaveUserResponse(
+
+    @field:SerializedName("message")
+    val message: String,
+
+    @field:SerializedName("user")
+    val userDto: UserDto,
+
+    @field:SerializedName("status")
+    val status: Int
+)
+
+data class UserDto(
+
+    @field:SerializedName("profileUrl")
+    val profileUrl: String,
+
+    @field:SerializedName("createdAt")
+    val createdAt: String,
+
+    @field:SerializedName("id")
+    val id: Int,
+
+    @field:SerializedName("email")
+    val email: String,
+
+    @field:SerializedName("username")
+    val username: String,
+
+    @field:SerializedName("updatedAt")
+    val updatedAt: String,
+
+    @field:SerializedName("Stores")
+    val stores: List<StoreDto>
+) {
+    fun toDomain() = User(id, username, email, profileUrl, stores.isNotEmpty())
+}
